ccceabed2d3af4ecd03d7897d78a85c8b9b7d032
Alex Kellas <alex@insphereltd.com>
2023-10-03 14:44:55 +0100

    detect method added

--- detect_aruco_images.py
+++ detect_aruco_images.py
@@ -8,39 +8,38 @@ import argparse
 import cv2
 import sys
 
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True, help="path to input image containing ArUCo tag")
+# ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="type of ArUCo tag to detect")
+# args = vars(ap.parse_args())
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to input image containing ArUCo tag")
-ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="type of ArUCo tag to detect")
-args = vars(ap.parse_args())
+def detect(image_path, type,):
+
+	print("Loading image...")
+	image = cv2.imread(image_path)
+	h,w,_ = image.shape
+	width=600
+	height = int(width*(h/w))
+	image = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
 
 
+	# verify that the supplied ArUCo tag exists and is supported by OpenCV
+	if ARUCO_DICT.get(type, None) is None:
+		print(f"ArUCo tag type '{type}' is not supported")
+		sys.exit(0)
 
-print("Loading image...")
-image = cv2.imread(args["image"])
-h,w,_ = image.shape
-width=600
-height = int(width*(h/w))
-image = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
+	# load the ArUCo dictionary, grab the ArUCo parameters, and detect
+	# the markers
+	print("Detecting '{}' tags....".format(type))
+	arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
+	arucoParams = cv2.aruco.DetectorParameters()
+	detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+	corners, ids, rejected = detector.detectMarkers(
+				image)
+	detected_markers = aruco_display(corners, ids, rejected, image)
+	cv2.imshow("Image", detected_markers)
 
+	# # Uncomment to save
+	cv2.imwrite("output_sample.png",detected_markers)
 
-# verify that the supplied ArUCo tag exists and is supported by OpenCV
-if ARUCO_DICT.get(args["type"], None) is None:
-	print(f"ArUCo tag type '{args['type']}' is not supported")
-	sys.exit(0)
-
-# load the ArUCo dictionary, grab the ArUCo parameters, and detect
-# the markers
-print("Detecting '{}' tags....".format(args["type"]))
-arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
-arucoParams = cv2.aruco.DetectorParameters()
-detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
-corners, ids, rejected = detector.detectMarkers(
-            image)
-detected_markers = aruco_display(corners, ids, rejected, image)
-cv2.imshow("Image", detected_markers)
-
-# # Uncomment to save
-cv2.imwrite("output_sample.png",detected_markers)
-
-cv2.waitKey(0)
+	cv2.waitKey(0)
