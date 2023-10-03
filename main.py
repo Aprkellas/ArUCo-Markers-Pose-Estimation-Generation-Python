f0c3c7963cc494a9d143ec55a7ebb170830defbb
Alex Kellas <alex@insphereltd.com>
2023-10-03 15:09:22 +0100

    main file complete w/ file selection

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from detect_aruco_images import detect
+from detect_aruco_images import detect#
+
+import tkinter as tk
+from tkinter import filedialog
 
 
 if __name__ == "__main__":
-    types = {1 : "1: DICT_4X4_100",
+    types = {1 : "DICT_4X4_100",
              2 : "DICT_4X4_250",
              3 : "DICT_5X5_100",
              4 : "DICT_5X5_250"}
+    
+    print(types)
     input = input("Please Enter an exact type number (e.g 1, 2 etc)")
 
-    type = types[input]
+    type = types.get(int(input))
 
+    print(f"You have chosen: {type}")
+
+    root = tk.Tk()
+    root.withdraw()
+
+    image_input = filedialog.askopenfilename(
+        title="Select an image file",
+        filetypes=[("Image files", "*.jpg *.jpeg *.png *.gif *.bmp *.ppm *.pgm")]
+    )
+
+    if not image_input:
+        print("No image file selected. Exiting.")
+    else:
+        print(f"Selected image file: {image_input}")
     
-    
+    detect(image_input, type)
+
