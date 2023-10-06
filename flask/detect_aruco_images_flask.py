3790e77bd62fd68fd59fb3c7f67dfed278efb50c
Aprkellas <alex.kellas.relton@gmail.com>
2023-10-06 12:52:51 +0100

    temporary file created so it can be displayed

--- flask/detect_aruco_images_flask.py
+++ flask/detect_aruco_images_flask.py
@@ -7,33 +7,51 @@ from utils import ARUCO_DICT, aruco_display
 import argparse
 import cv2
 import sys
+import tempfile
+import os
 
 
-def detect(image_path):
-    print("Loading image...")
-    image = cv2.imread(image_path)
-    h, w, _ = image.shape
-    width = 600
-    height = int(width * (h / w))
-    image = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
-    detected = False
-    detected_markers = None 
+def detect(uploaded_image):
+    try:
+        # Create a temporary directory to store the uploaded image
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Generate a temporary file path
+            temp_image_path = os.path.join(temp_dir, "uploaded_image.jpg")
 
-    for tag_type, aruco_dict_value in ARUCO_DICT.items():
-        if not detected:
-            try:
-                # Load the ArUco dictionary, grab the ArUco parameters, and detect the markers
-                print(f"Detecting '{tag_type}' tags...")
-                arucoDict = cv2.aruco.getPredefinedDictionary(aruco_dict_value)
-                arucoParams = cv2.aruco.DetectorParameters()
-                detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
-                corners, ids, rejected =  detector.detectMarkers(
-				image)
+            # Save the uploaded image to the temporary file
+            uploaded_image.save(temp_image_path)
 
-                if len(corners) > 0:
-                    detected_markers, detected = aruco_display(corners, ids, rejected, image)
+            # Read the image using cv2.imread
+            image = cv2.imread(temp_image_path)
 
-            except Exception as e:
-                print(f"Error detecting '{tag_type}' tags: {e}")
+            if image is not None:
+                print("Loading image...")
+                h, w, _ = image.shape
+                width = 600
+                height = int(width * (h / w))
+                image = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
+                detected = False
+                detected_markers = None
 
-    return detected_markers
+            for tag_type, aruco_dict_value in ARUCO_DICT.items():
+                if not detected:
+                    try:
+                        # Load the ArUco dictionary, grab the ArUco parameters, and detect the markers
+                        print(f"Detecting '{tag_type}' tags...")
+                        arucoDict = cv2.aruco.getPredefinedDictionary(aruco_dict_value)
+                        arucoParams = cv2.aruco.DetectorParameters()
+                        detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+                        corners, ids, rejected =  detector.detectMarkers(
+                        image)
+
+                        if len(corners) > 0:
+                            detected_markers, detected = aruco_display(corners, ids, rejected, image)
+
+                    except Exception as e:
+                        print(f"Error detecting '{tag_type}' tags: {e}")
+
+            return detected_markers
+        
+    except Exception as e:
+        print(f"error: {e}")
+        return None
