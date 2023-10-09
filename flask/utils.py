f0ca43b020251a7096adebce6fca32d12014b761
Alex Kellas <alex@insphereltd.com>
2023-10-09 10:59:00 +0100

    slight reorder of aruco dict

--- flask/utils.py
+++ flask/utils.py
@@ -1,10 +1,10 @@
 import cv2
 
 ARUCO_DICT = {
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
 	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
 	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
 	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
 	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
 	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
 	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
